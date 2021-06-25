16b0269e5887a3bbe28b165a873c25badaa7a539
unknown <alicegetmanchuk@gmail.com>
2021-06-25 10:51:27 -0500

    first commit- nav bar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+// import {Navbar, Nav}from './react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title:'Home',
+        url:'home',
+        cName: 'nav-links'
+    },
+    {
+        title:'Education',
+        url:'education',
+        cName: 'nav-links'
+    },
+    {
+        title:'Experience',
+        url:'experience',
+        cName: 'nav-links'
+    },
+    {
+        title:'Extracurricular',
+        url:'extracurricular',
+        cName: 'nav-links'
+    },
+    {
+        title:'Contact',
+        url:'contact',
+        cName: 'nav-links-mobile'
+    }
+]
